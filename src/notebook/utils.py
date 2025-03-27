bbc5fd8afd0db9ec0be27da50b3805c4938db1de
Carlos David López Hinojosa <backupcarlos18@gmail.com>
2025-03-27 09:22:44 +0100

    chama

--- src/notebook/utils.py
+++ src/notebook/utils.py
@@ -2,37 +2,37 @@ import random as rnd
 import math
 import numpy as np
 
-def random_coeficients(n: int) -> np.array:
+def random_coeficients(n: int) -> list:
     """
     Genera un array de coeficientes aleatorios.
     Args:
         n (int): Número de coeficientes.
     Returns:
-        np.array: Array con valores aleatorios entre 0 y 1.
+        list: Array con valores aleatorios entre 0 y 1.
     """
-    return np.array([rnd.random() for _ in range(n)])
+    return [rnd.random() for _ in range(n)]
 
-def initial_population(pop_size, n) -> np.array:
+def initial_population(pop_size: int, n: int) -> list:
     """
     Genera una población inicial de coeficientes aleatorios.
     Args:
         pop_size (int): Tamaño de la población.
         n (int): Número de coeficientes por individuo.
     Returns:
-        np.array: Matriz de coeficientes aleatorios.
+        list: Matriz de coeficientes aleatorios.
     """
-    return np.array([random_coeficients(n) for _ in range(pop_size)])
+    return [random_coeficients(n) for _ in range(pop_size)]
 
-def get_data(lower: int = 0, upper: int = 15) -> np.array:
+def get_data(lower: int = 0, upper: int = 15) -> list[list]:
     """
     Obtiene un subconjunto de datos de prueba.
     Args:
         lower (int, opcional): Índice inferior del subconjunto. Por defecto 0.
         upper (int, opcional): Índice superior del subconjunto. Por defecto 15.
     Returns:
-        np.array: Subconjunto de datos en el rango especificado.
+        list[list]: Subconjunto de datos en el rango especificado.
     """
-    return np.array([
+    return [
         [0, 3.490342957],
         [0.1, 3.649406057],
         [0.2, 3.850310157],
@@ -48,26 +48,28 @@ def get_data(lower: int = 0, upper: int = 15) -> np.array:
         [-0.82, 0.0372406176],
         [-1.98, -0.2501897336],
         [-1.99, 0.4626335969]
-    ][lower:upper])
+    ][lower:upper]
 
-def f(x: float, coeficients: np.array) -> float:
+def f(x: float, coeficients: list) -> float:
     """
     Calcula el valor de una función basada en coeficientes.
     Args:
         x (float): Valor de entrada.
-        coeficients (np.array): Array de coeficientes.
+        coeficients (list): Array de coeficientes.
     Returns:
         float: Resultado de la función.
     """
     return math.exp(coeficients[0]) + sum([coeficients[i] * x ** i for i in range(1, len(coeficients))])
 
-def error(coeficients, data) -> float:
+data = get_data()
+def error(coeficients: list) -> float:
     """
     Calcula el error cuadrático entre valores estimados y valores reales.
     Args:
-        coeficients (np.array): Coeficientes del modelo.
-        data (list of tuples): Datos en pares (x, y).
+        coeficients (list): Coeficientes del modelo.
+        data (list of list): Datos en pares (x, y).
     Returns:
         float: Error cuadrático total.
     """
+    global data
     return sum([(f(x, coeficients) - y) ** 2 for x, y in data])
