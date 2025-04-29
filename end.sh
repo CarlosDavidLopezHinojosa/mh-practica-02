c844b55e566cfd69f7f9b72a154727a72683e534
Carlos David López Hinojosa <backupcarlos18@gmail.com>
2025-04-29 11:57:38 +0200

    final

--- end.sh
+++ end.sh
@@ -1,2 +1,4 @@
+echo "Borrando los directorios de caché..."
 rm -rf src/*/__pycache__
+echo "Borrando el directorio de entorno virtual..."
 rm -rf venv/
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python3 -m venv env
-source env/bin/activate
+python3 -m venv venv
+source venv/bin/activate
 pip3 install -r requirements.txt
 
 streamlit run src/view.py
